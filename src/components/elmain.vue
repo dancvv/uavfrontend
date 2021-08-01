979a0a2566132cb5ae1b0e6c1b8c13cc79c4e2db
“laptopCarbon” <uquantum@hotmail.com>
2021-08-01 22:49:46 +0800

    无人机地图界面展示

--- src/components/elmain.vue
+++ src/components/elmain.vue
@@ -1,19 +1,42 @@
 <template>
   <div>
     <el-main>
-      主页内容<br>
-      <img src="/public/uav.png" width="10px">
-
+      <div id="app">
+        <amap :zoom="18" :center="position1">
+          <amap-marker
+              :position="position1"
+              :offset="[-22, -40]"
+              :label="{
+          // content: '使用自定义Icon的Marker',
+          direction: 'bottom',
+        }"
+              :icon="uavIcon"
+          />
+              draggable
+          />
+        </amap>
+      </div>
     </el-main>
   </div>
 </template>
 
 <script>
 export default {
-  name: "elmain"
+  name: "elmain",
+  data() {
+    return {
+      position1: [121.81206,39.084716]
+    };
+  }
 }
 </script>
 
 <style scoped>
-
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  width: 900px;
+  height: 490px;
+}
 </style>
