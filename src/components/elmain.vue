1db4cac5853a37e8454e9098bc76800e239512a1
“laptopCarbon” <uquantum@hotmail.com>
2021-08-01 23:37:15 +0800

    首页地图显示

--- src/components/elmain.vue
+++ src/components/elmain.vue
@@ -2,7 +2,7 @@
   <div>
     <el-main>
       <div id="app">
-        <amap :zoom="18" :center="position1">
+        <amap :zoom="16" :center="position4">
           <amap-marker
               :position="position1"
               :offset="[-22, -40]"
@@ -10,7 +10,34 @@
           // content: '使用自定义Icon的Marker',
           direction: 'bottom',
         }"
-              :icon="uavIcon"
+              :icon="uav.icon"
+          />
+          <amap-marker
+              :position="position2"
+              :offset="[-22, -40]"
+              :label="{
+          // content: '使用自定义Icon的Marker',
+          direction: 'bottom',
+        }"
+              :icon="uav.icon"
+          />
+          <amap-marker
+              :position="position3"
+              :offset="[-22, -40]"
+              :label="{
+          // content: '使用自定义Icon的Marker',
+          direction: 'bottom',
+        }"
+              :icon="uav.icon"
+          />
+          <amap-marker
+              :position="position4"
+              :offset="[-22, -40]"
+              :label="{
+          // content: '使用自定义Icon的Marker',
+          direction: 'bottom',
+        }"
+              :icon="uav.icon"
           />
               draggable
           />
@@ -21,11 +48,24 @@
 </template>
 
 <script>
+import uavIcon from "/public/uav.png"
 export default {
   name: "elmain",
   data() {
     return {
-      position1: [121.81206,39.084716]
+      position1: [121.81206,39.084716],
+      position2: [121.81300,39.08371],
+      position3: [121.822,39.0847],
+      position4: [121.818,39.083],
+      uav: {
+
+
+        icon: {
+          image: uavIcon,
+          imageSize: [50, 60],
+        },
+        offset: [-13, -25],
+      },
     };
   }
 }
