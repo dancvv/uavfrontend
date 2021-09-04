91e3354571c7882d4f2b5fd3fe9472bd6dd295d7
laptopdot <uquantum@hotmail.com>
2021-09-05 02:23:09 +0800

    分离自定义marker设置栏目，剥离点击添加marker的方法

--- src/components/homemap/mapboxView.vue
+++ src/components/homemap/mapboxView.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="mapbox">
-  <el-radio-group class="mapStyle" v-model="map.style" @change="changeMap">
+  <el-radio-group class="mapStyle" v-model="map.style" @change="changeStyle">
     <el-radio-button label="mapbox://styles/mapbox/streets-v9" size="medium" >平面地图</el-radio-button>
     <el-radio-button label="mapbox://styles/mapbox/satellite-v9" size="medium" >卫星地图</el-radio-button>
     <el-radio-button label="mapbox://styles/mapbox/outdoors-v10" size="medium" >户外地图</el-radio-button>
@@ -8,11 +8,14 @@
   <el-card class="card-box">
     <div slot="header">选项设置</div>
     <el-button type="primary" class="mapgroup" @click="placePoint">{{poly.edit?'停止绘制':'开始绘制'}}</el-button>
-    <el-button type="primary" class="mapgroup" @click="changeMap">地图更改</el-button>
-    <el-input class="mapgroup" >{{location.lat}}</el-input>
+    <el-button type="primary" class="mapgroup" @click="uploadData">上传数据</el-button>
+    <el-button type="primary" class="mapgroup" @click="showMarker">自定义图标</el-button>
+    <el-input class="mapgroup" >{{location}}</el-input>
   </el-card>
 
-  <div id="map-view"></div>
+  <div id="map-view">
+    <div class="marker"></div>
+  </div>
 </div>
 
 
@@ -49,14 +52,39 @@ export default {
         center: this.map.center,
         zoom: this.map.zoom,
       });
-      // element marker元素
-      let el=document.createElement('div')
-      el.className='marker'
-      el.style.backgroundImage='`url(/public/uav48.svg)`'
-      el.style.width='48px'
-      el.style.height='48px'
-      el.style.backgroundSize = '100%';
-      new mapboxgl.Marker(el).setLngLat(this.location).addTo(map)
+      // map.on('click',(e)=>{
+      //   if (!this.poly.edit){
+      //     return
+      //   }
+      //   //临时位置数组
+      //   let temp=[]
+      //   temp[0]=e.lngLat.lng
+      //   temp[1]=e.lngLat.lat
+      //   console.log(temp)
+      //   this.poly.paths.push(e.lngLat)
+      //   console.log(this.poly.paths)
+      //   //根据点击位置放置一个图标
+      //   new mapboxgl.Marker().setLngLat(temp).addTo(map)
+      // });
+      // map.setStyle(this.map.style)
+      // console.log(this.map.style)
+      map.addControl(new mapboxgl.NavigationControl())
+      return map;
+    },
+    //改变地图样式
+    changeStyle(){
+      map.setStyle(this.map.style)
+      console.log(this.map.style)
+    },
+    //上传数据
+    async uploadData() {
+      // const map=this.init()
+      const {data: res} = await this.$http.post('compute/depotData', this.poly.paths)
+      console.log(res)
+
+    },
+    placePoint(){
+      this.poly.edit=!this.poly.edit
       map.on('click',(e)=>{
         if (!this.poly.edit){
           return
@@ -69,26 +97,27 @@ export default {
         this.poly.paths.push(e.lngLat)
         console.log(this.poly.paths)
         //根据点击位置放置一个图标
-        new mapboxgl.Marker(el).setLngLat(temp).addTo(map)
+        new mapboxgl.Marker().setLngLat(temp).addTo(map)
       });
-      map.setStyle(this.map.style)
-      console.log(this.map.style)
-      map.on('load',()=>{
-        map.setStyle(this.map.style)
-      })
-      map.addControl(new mapboxgl.NavigationControl())
-      return map;
     },
+    showMarker(){
+      // element marker元素
+      // const el = document.createElement('div');
+      // el.id = 'marker';
+      // const marker=new mapboxgl.Marker(el).setLngLat([121.81135905402766, 39.084797545212155]).addTo(map)
 
-    async changeMap() {
-      // const map=this.init()
-      const {data: res} = await this.$http.post('compute/depotData', this.poly.paths)
-      console.log(res)
-      map.setStyle(this.map.style)
-      console.log(this.map.style)
-    },
-    placePoint(){
-      this.poly.edit=!this.poly.edit
+
+      const el = document.createElement('div');
+      el.className = 'marker';
+      // el.style.backgroundImage = "url(https://placekitten.com/g/50/50)";
+      // el.style.width = '48px';
+      // el.style.height = '48px';
+      // el.style.backgroundSize = '100%';
+// Add markers to the map.
+      const marker=new mapboxgl.Marker(el)
+          .setLngLat([121.81135905402766, 39.084797545212155])
+          .addTo(map);
+      console.log(marker)
     }
   },
 }
@@ -118,5 +147,12 @@ export default {
   z-index: 1;
   margin-top: 20px;
   margin-left: 20px;
+}
+.marker{
+  background-image: url('https://placekitten.com/g/50/50');
+  background-size: 100%;
+  width: 48px;
+  height: 48px;
+
 }
 </style>
