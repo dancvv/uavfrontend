fdb80f924266f16249948cad6c379cfdef2fd996
laptopdot <uquantum@hotmail.com>
2021-10-19 23:37:44 +0800

    框架板块

--- src/components/uav/controlinstance.vue
+++ src/components/uav/controlinstance.vue
@@ -6,20 +6,52 @@
     <el-breadcrumb-item>任务执行</el-breadcrumb-item>
     <el-breadcrumb-item>当前路线</el-breadcrumb-item>
   </el-breadcrumb>
-  <el-card>
-    <div slot="header">
-      <h3>状态初始化</h3>
-    </div>
-    <div>
-      <p>无人金</p>
-    </div>
-  </el-card>
+  <el-row>
+    <el-col :span="11">
+      <el-card class="cardstate">
+        <h3 class="header">状态初始化</h3>
+
+
+        <el-popover
+            placement="top-start"
+            title="标题"
+            width="200"
+            trigger="hover"
+            content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。"></el-popover>
+        <el-button slot="reference">tanhcud</el-button>
+        <p>dfjkejfke</p>
+        <el-button>djfkejfek</el-button>
+      </el-card>
+    </el-col>
+    <el-col :span="11">
+      <el-card class="cardstate">
+        <div>
+          <h3 class="header">动作指令</h3>
+        </div>
+      </el-card>
+    </el-col>
+  </el-row>
+  <el-row>
+    <el-col :span="9">
+      <el-card class="cardstop">
+        <h3 class="header">停止</h3>
+      </el-card>
+    </el-col>
+  </el-row>
+
+
+
 </div>
 </template>
 
 <script>
 export default {
-  name: "controlinstance"
+  name: "controlinstance",
+  data(){
+    return{
+      visible:false
+    }
+  }
 }
 </script>
 
@@ -27,8 +59,25 @@ export default {
 .breadcrumb{
   margin: 20px;
 }
-.el-card{
+
+.cardstate{
   margin-left: 30px;
-  margin-right: 30px;
+  background-color: #007aff;
+  color: white;
+  display: flex;
+  justify-content: start ;
+  margin-bottom: 30px;
+}
+.el-divider{
+  color: white;
+}
+.header{
+  margin-top: -5px;
+  color: white;
+}
+.cardstop{
+  margin-left: 30px;
+  background-color: #ff3b30;
+
 }
 </style>
